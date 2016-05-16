1f9582eb3ac47b425e7ea5051b16f4742792d7fa
Wei Wei <wwei1@ualberta.ca>
2016-05-16 17:45:26 -0600

    make datalist sortable and searchable, but data cannot be aligned

--- datalist.php
+++ datalist.php
@@ -6,55 +6,92 @@
     <!--<script type="text/javascript" src="easyui/jquery.min.js"></script>-->
     <!--<script type="text/javascript" src="easyui/jquery.easyui.min.js"></script>-->
     <script type="text/javascript" src="http://code.jquery.com/jquery-1.4.4.min.js"></script>
-    <script type="text/javascript" src="http://www.jeasyui.com/easyui/jquery.easyui.min.js"></script>
+    <script type="text/javascript" src="easyui/jquery.easyui.min.js"></script>
+    
 
-	<table class="easyui-datagrid" title="OSSLRD" style="width:auto;height:250px"
-            toolbar="#toolbar" pagination="true" iconCls="icon-save" 
-            rownumbers="true" data-options="remoteSort:false,singleSelect:true,collapsible:true,url:'Table.php',method:'get'">
+	<table id="dg" class="easyui-datagrid" title="OSSLRD" style="width:auto;height:auto"
+            toolbar="#toolbar" pagination="true"  
+            rownumbers="true" data-options="remoteSort:false,singleSelect:true,collapsible:true,url:'Table.php',method:'get',remoteFilter:false">
         <thead>
             <tr>
-                <th data-options="field:'id',width:80, align:'center'" sortable="true">ID</th>
-                <th data-options="field:'title',width:100, align:'center'">Title</th>
-                <th data-options="field:'format',width:80,align:'center'">Format</th>
-                <th data-options="field:'year',width:80,align:'center'" sortable="true">Year</th>
+                <th data-options="field:'id'" sortable="true"  >ID</th>
+                <th data-options="field:'title'" sortable="true"   >Title</th>
+                <th data-options="field:'format'" sortable="true"   >Format</th>
+                <th data-options="field:'year'" sortable="true"   >Year</th>
 
-                <th data-options="field:'author1',width:80, align:'center'">Author1</th>
-                <th data-options="field:'role1',width:80,align:'center'">Role1</th>
-                <th data-options="field:'affiliation1',width:80,align:'center'">Affiliation1</th>
+                <th data-options="field:'author1'" sortable="true"   >Author1</th>
+                <th data-options="field:'role1'" sortable="true"   >Role1</th>
+                <th data-options="field:'affiliation1'" sortable="true"   >Affiliation1</th>
 
-                <th data-options="field:'author2',width:80, align:'center'">Author2</th>
-                <th data-options="field:'role2',width:80,align:'center'">Role2</th>
-                <th data-options="field:'affiliation2',width:80,align:'center'">Affiliation2</th>
+                <th data-options="field:'author2'" sortable="true"   >Author2</th>
+                <th data-options="field:'role2'" sortable="true"  >Role2</th>
+                <th data-options="field:'affiliation2'" sortable="true"  >Affiliation2</th>
 
-                <th data-options="field:'author3',width:80, align:'center'">Author3</th>
-                <th data-options="field:'role3',width:80,align:'center'">Role3</th>
-                <th data-options="field:'affiliation3',width:80,align:'center'">Affiliation3</th>
+                <th data-options="field:'author3'" sortable="true"  >Author3</th>
+                <th data-options="field:'role3'" sortable="true"  >Role3</th>
+                <th data-options="field:'affiliation3'" sortable="true"  >Affiliation3</th>
 
-                <th data-options="field:'author4',width:80, align:'center'">Author4</th>
-                <th data-options="field:'role4',width:80,align:'center'">Role4</th>
-                <th data-options="field:'affiliation4',width:80,align:'center'">Affiliation4</th>
+                <th data-options="field:'author4'" sortable="true"  >Author4</th>
+                <th data-options="field:'role4'" sortable="true"  >Role4</th>
+                <th data-options="field:'affiliation4'" sortable="true"  >Affiliation4</th>
 
-                <th data-options="field:'author5',width:80, align:'center'">Author5</th>
-                <th data-options="field:'role5',width:80,align:'center'">Role5</th>
-                <th data-options="field:'affiliation5',width:80,align:'center'">Affiliation5</th>
+                <th data-options="field:'author5'" sortable="true"  >Author5</th>
+                <th data-options="field:'role5'" sortable="true"  >Role5</th>
+                <th data-options="field:'affiliation5'" sortable="true"  >Affiliation5</th>
 
-                <th data-options="field:'country',width:80, align:'center'">Country</th>
-                <th data-options="field:'conference',width:80,align:'center'">Conference</th>
-                <th data-options="field:'location',width:80,align:'center'">Location</th>
+                <th data-options="field:'country'" sortable="true"  >Country</th>
+                <th data-options="field:'conference'" sortable="true"  >Conference</th>
+                <th data-options="field:'location'" sortable="true"  >Location</th>
 
-                <th data-options="field:'method1',width:80, align:'center'">Method1</th>
-                <th data-options="field:'method2',width:80,align:'center'">Method2</th>
-                <th data-options="field:'method3',width:80,align:'center'">Method3</th>
+                <th data-options="field:'method1'" sortable="true"  >Method1</th>
+                <th data-options="field:'method2'" sortable="true"  >Method2</th>
+                <th data-options="field:'method3'" sortable="true"  >Method3</th>
 
-                <th data-options="field:'source',width:80,align:'center'">Source</th>
-                <th data-options="field:'abstract',width:80,align:'center'">Abstract</th>
+                <th data-options="field:'source'" sortable="true"  >Source</th>
+                <th data-options="field:'abstract'" sortable="true"  >Abstract</th>
 
-                <th data-options="field:'class1',width:80, align:'center'">Class1</th>
-                <th data-options="field:'class2',width:80,align:'center'">Class2</th>
-                <th data-options="field:'class3',width:80,align:'center'">Class3</th>
+                <th data-options="field:'class1'" sortable="true"  >Class1</th>
+                <th data-options="field:'class2'" sortable="true"  >Class2</th>
+                <th data-options="field:'class3'" sortable="true"  >Class3</th>
             </tr>
         </thead>
     </table>
+
+
+    <script type="text/javascript" src="easyui/datagrid-filter.js"></script>
+    <script type="text/javascript">
+        $(function(){
+             var dg = $('#dg');
+            dg.datagrid();  // create datagrid
+            dg.datagrid('enableFilter', [{
+                field:'year',
+                type:'numberbox',
+                options:{precision:1},
+                op:['equal','notequal','less','greater']
+            },{
+                field:'format',
+                type:'combobox',
+                options:{
+                    panelHeight:'auto',
+                    data:[{value:'',text:'All'},{value:'C',text:'Conference(C)'},{value:'J',text:'Journal(J)'},{value:'R',text:'Research Paper(R)'},{value:'DT',text:'Dissertation/Thesis'}],
+                    onChange:function(value){
+                        if (value == ''){
+                            dg.datagrid('removeFilterRule', 'format');
+                        } else {
+                            dg.datagrid('addFilterRule', {
+                                field: 'format',
+                                op: 'equal',
+                                value: value
+                            });
+                        }
+                        dg.datagrid('doFilter');
+                    }
+                }
+            }]);
+            
+        });
+
+    </script>
 
 	
 
