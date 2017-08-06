f11cac8c355e990c9f761da715bf9ce1d0c89c7a
Wei Wei <wwei1@ualberta.ca>
2017-08-06 17:18:25 -0600

    ““

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 	<div class="footer">
 		<div class="footer-inner">
 
-		<p>This website is supported by <a href="http://www.elementaryed.ualberta.ca/" target="_blank"> Department of Elementary Education</a> of <a href="http://www.ualberta.ca/" target="_blank"> University of Alberta</a></p>
+		<p>This website is supported by <a href="http://www.elementaryed.ualberta.ca/" target="_blank"> Faculty of Elementary Education</a> of <a href="http://www.ualberta.ca/" target="_blank"> University of Alberta</a></p>
 
 		</div>
 	</div>
